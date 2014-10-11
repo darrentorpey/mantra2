c9227f82d36c07bbc2e35c614d2e2305d712e8cf
Darren Torpey <darren.torpey@gmail.com>
2014-10-11 19:02:05 -0400

    Adding a quick demo w/ web server

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,27 @@ module.exports = function(grunt) {
         configFile: ROOT_PATH + 'config/karma.conf.js',
         singleRun: true
       }
+    },
+
+    'http-server': {
+        'dev': {
+            // the server root directory
+            root: 'demos/hello/index.html',
+
+            port: 8282,
+            // port: function() { return 8282; }
+
+            host: "127.0.0.1",
+
+            showDir : true,
+            autoIndex: true,
+
+            // server default file extension
+            ext: "html",
+
+            // run in parallel with other tasks
+            runInBackground: false
+        }
     }
   });
 
@@ -43,6 +64,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-http-server');
 
   // By default, lint
   grunt.registerTask('default', ['jshint', 'karma:continuous']);
