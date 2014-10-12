5b72d7da3c4476c5b05e4f69588c063ee6b13788
Darren Torpey <darren.torpey@gmail.com>
2014-10-11 20:01:55 -0400

    Some code

--- app/canvas.js
+++ app/canvas.js
@@ -1,11 +1,28 @@
 (function() {
 'use strict';
 
-var module = window.Mantra.Canvas = {},
-    document = window.document;
+var document = window.document;
 
-module.create = function() {
-    document.createElement( 'canvas' );
+function MantraCanvas() {
+    this.d_canvas = MantraCanvas.create();
+
+    this.context = this.d_canvas.getContext('2d');
+}
+
+MantraCanvas.prototype.getCanvas = function() {
+    return this.d_canvas;
 };
+
+MantraCanvas.create = function() {
+    return document.createElement( 'canvas' );
+};
+
+MantraCanvas.init = function() {
+    var canvas = new MantraCanvas();
+
+    document.body.appendChild( canvas.getCanvas() );
+};
+
+window.Mantra.Canvas = MantraCanvas;
 
 })();
